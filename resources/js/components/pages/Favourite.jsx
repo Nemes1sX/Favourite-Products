f54acd37d2667410c57e4f60193fb733c1438e3d
Nemes1sX <kazstonk@gmail.com>
2023-03-21 01:35:37 +0200

    Added product and favourite product list

--- resources/js/components/pages/Favourite.jsx
+++ resources/js/components/pages/Favourite.jsx
@@ -1,8 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 export default function Favourite() {
+    const [favouriteProducts, setFavouriteProducts] = useState([]);
+
+    useEffect(() => {
+        fetchFavouriteProducts();
+    }, []);
+
+    function fetchFavouriteProducts() {
+        console.log('Fetch products');
+        axios.get('http://localhost:8000/api/favourite/index?user_id=10')
+            .then(response => response.data)
+            .then(data => {
+                    console.log(data[0]);
+                    setFavouriteProducts(data);
+                    console.log(favouriteProducts);
+                }
+            );
+    }
+
     return (
-      <h1>Favourite products page</h1>
+        <>
+            <div className="container">
+                <div className="row">
+                    <h1 className="text-center">Favourite products</h1>
+                    {
+                        favouriteProducts.length > 0 &&
+                        favouriteProducts.map((product, key) =>
+                            <div className="col-lg-4 col-md-6 mb-4" key={key}>
+                                <div className="card h-100">
+                                    <div className="card-body">
+                                        <h4 className="card-title">
+                                            <a href="#">{product.name}</a>
+                                        </h4>
+                                        <h5>$ {product.price}</h5>
+                                        <p className="card-text">{product.description}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                    {favouriteProducts.length === 0 && <h2 className="text-center">No products</h2>}
+                </div>
+            </div>
+        </>
     );
 }
--- resources/js/components/pages/Products.jsx
+++ resources/js/components/pages/Products.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 export default function Products() {
-    console.log('Hello');
+    const [products, setProducts] = useState([]);
+    const url = window.location.href;
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    function fetchProducts()
+    {
+        axios.get('http://localhost:8000/api/index')
+            .then(response => {
+                const fetchedProducts = response.data.data;
+                setProducts(fetchedProducts);
+            }
+        );
+    }
 
     return (
-        <h1>Products page</h1>
+        <>
+        <div className="container">
+            <div className="row">
+                <h1 className="text-center">Products list</h1>
+                {
+                    products.length > 0 &&
+                    products.map((product, key) =>
+                            <div className="col-lg-4 col-md-6 mb-4" key={key}>
+                        <div className="card h-100">
+                            <div className="card-body">
+                                <h4 className="card-title">
+                                    {product.name}
+                                </h4>
+                                <h5>$ {product.price}</h5>
+                                <p className="card-text">{product.description}</p>
+                            </div>
+                        </div>
+                        </div>
+                        )}
+                { products.length === 0 && <h2 className="text-center">No products</h2>}
+            </div>
+        </div>
+        </>
     );
 }
