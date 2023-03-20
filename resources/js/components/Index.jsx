bcc9f87767e9f2aa0c620259fbbd9ab9b28f7873
Nemes1sX <kazstonk@gmail.com>
2023-03-21 01:34:52 +0200

    Fix react routing

--- resources/js/components/Index.jsx
+++ resources/js/components/Index.jsx
@@ -3,20 +3,17 @@ import Layout  from './Layout';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Products from "./pages/Products";
 import Favourite from "./pages/Favourite";
+import Login from "./pages/Login";
 
 export default function Index() {
 
     return (
        <Layout>
-        <Fragment>
-            <Routes>
-                <Route path="/" exact element={<Products />}>
-                    <Route path="products" element={<Products />}>
-                        <Route path="favourite" element={<Favourite />} />
-                    </Route>
-                </Route>
-            </Routes>
-        </Fragment>
+        <Routes>
+            <Route path="/" element={<Products />} />
+            <Route path="/favourite" element={<Favourite />} />
+            <Route path="/login" element={<Login />} />
+        </Routes>
        </Layout>
     );
 }
